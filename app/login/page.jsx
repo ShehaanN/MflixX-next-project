e146d99f843a406955281cc26cc5077a5b71a59b
ShehaanN <shehannadeesha2580@gmail.com>
2025-03-02 17:58:23 +0530

    add inline style

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,7 +3,7 @@ import LoginForm from "./login-form";
 //Server component for SSR
 export default function Loginpage() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen text-black ">
+    <div className="flex flex-col justify-center items-center min-h-screen text-black " style={{backgroundColor:"#e8f2f1"}}>
       <LoginForm title="Sign in to your account" />
     </div>
   );
