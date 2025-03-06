e491cbe1260c4478bcc1d0fc71a8b8f5c40b118c
ShehaanN <shehannadeesha2580@gmail.com>
2025-03-06 22:29:09 +0530

    Update login form title

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,8 +3,11 @@ import LoginForm from "./login-form";
 //Server component for SSR
 export default function Loginpage() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen text-black " style={{backgroundColor:"#e8f2f1"}}>
-      <LoginForm title="Sign in to your account" />
+    <div
+      className="flex flex-col justify-center items-center min-h-screen text-black "
+      style={{ backgroundColor: "#e8f2f1" }}
+    >
+      <LoginForm title="Sign in to MflixX" />
     </div>
   );
 }
