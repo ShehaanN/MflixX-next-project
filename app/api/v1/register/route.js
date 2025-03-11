0e08870a05d280e14ad3f91425489ad963f33dda
ShehaanN <shehannadeesha2580@gmail.com>
2025-03-12 00:11:23 +0530

    backend data validation

--- app/api/v1/register/route.js
+++ app/api/v1/register/route.js
@@ -4,5 +4,16 @@ export const POST = async (req) => {
   const { name, email, password } = await req.json();
   console.log(name, email, password);
 
+  if (!name || !email || !password) {
+    return NextResponse.json(
+      {
+        error: "name, email, and password are required",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
   return NextResponse.json({ success: true, name, email });
 };
