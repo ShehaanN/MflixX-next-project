b89ea0964a959f66e5edae13be96ca4834a4455e
ShehaanN <shehannadeesha2580@gmail.com>
2025-03-11 23:58:26 +0530

    basic frontend validation

--- app/api/v1/register/route.js
+++ app/api/v1/register/route.js
@@ -1,14 +1,8 @@
 import { NextResponse } from "next/server";
 
 export const POST = async (req) => {
-  const request = await req.json();
-  console.log(request);
+  const { name, email, password } = await req.json();
+  console.log(name, email, password);
 
-  //Bind Database
-  // Find the user in the database
-  // If the user exists, check the password validity
-  // If the password is valid, return the password with torken
-  // If the password is invalid, return an error message
-
-  return NextResponse.json({ success: true, username: "ShehaanN" });
+  return NextResponse.json({ success: true, name, email });
 };
