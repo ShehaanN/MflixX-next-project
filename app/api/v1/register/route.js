b97272ab28313b99bd2580c6f3cff9347edf2337
ShehaanN <shehannadeesha2580@gmail.com>
2025-03-12 21:49:46 +0530

    add user registration error handling

--- app/api/v1/register/route.js
+++ app/api/v1/register/route.js
@@ -1,19 +1,43 @@
 import { NextResponse } from "next/server";
+import clientPromise from "@/app/libs/mongodb";
 
 export const POST = async (req) => {
-  const { name, email, password } = await req.json();
-  console.log(name, email, password);
+  try {
+    const { name, email, password } = await req.json();
 
-  if (!name || !email || !password) {
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        {
+          error: "name, email, and password are required",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const client = await clientPromise();
+    // MflixX is the database name
+    const db = client.db("sample_mflix");
+
+    const existingUser = await db.collection("users").findOne({ email });
+    console.log("Is existing user", existingUser);
+
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          error: "User with this email already exists.",
+        },
+        { status: 409 }
+      );
+    }
+
+    return NextResponse.json({ success: true, name, email });
+  } catch (error) {
+    console.log("MONGODB Error", error);
     return NextResponse.json(
-      {
-        error: "name, email, and password are required",
-      },
-      {
-        status: 400,
-      }
+      { error: "Internal server error" },
+      { status: 500 }
     );
   }
-
-  return NextResponse.json({ success: true, name, email });
 };
